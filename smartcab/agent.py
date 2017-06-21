bb1a8aa1dabee2ef68bec9887e4d591a978f5ce7
interlove <interlove@me.com>
2017-06-22 01:05:07 +0800

    Update per the review comments

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -140,13 +140,17 @@ class LearningAgent(Agent):
                 action = random.choice(self.valid_actions)
             else:
                 max_Q_value = None
+                actionList = []
                 for (state_action, q_value) in self.Q[state].items():
                     if max_Q_value == None:
-                        action = state_action
+                        actionList = [state_action]
                         max_Q_value = q_value
                     elif q_value > max_Q_value:
-                        action = state_action
+                        actionList = [state_action]
                         max_Q_value = q_value
+                    elif q_value == max_Q_value:
+                        actionList.append(state_action)
+                action = random.choice(actionList)
 
                 print "take {} action, Q-value {}, waypoint {}".format(action, max_Q_value, self.next_waypoint)
  
@@ -218,7 +222,7 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env, update_delay=.01, display=False, log_metrics=True, optimized=True)
+    sim = Simulator(env, update_delay=.01, display=False, log_metrics=False, optimized=True)
     
     ##############
     # Run the simulator
